cdcba07184df19028ea1dfd2d50d805ad1ac87b2
Johann Knechtel <johann@nyu.edu>
2019-06-02 20:32:07 +0400

    added further files to enable compilation

---
+++ source/src/bench.tab.hh
@@ -0,0 +1,86 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_BENCH_TAB_HH_INCLUDED
+# define YY_YY_BENCH_TAB_HH_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    INPUT = 258,
+    OUTPUT = 259,
+    LPAREN = 260,
+    RPAREN = 261,
+    COMMA = 262,
+    EQUALS = 263,
+    ID = 264
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 23 "bench.y" /* yacc.c:1909  */
+
+    std::string             *id;
+    ast_n::input_decl_t     *input_stm;
+    ast_n::output_decl_t    *output_stm;
+    ast_n::gate_decl_t      *gate_stm;
+    ast_n::statement_t      *stm;
+    ast_n::statements_t     *statements;
+    ast_n::gate_inputs_t    *gate_inputs;
+
+#line 74 "bench.tab.hh" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_BENCH_TAB_HH_INCLUDED  */
